478f47dd62eaff8e8e337835a8907257bcaf6584
Raul Carini <r.carini2003@gmail.com>
2022-10-24 19:47:53 +0200

    add replay

--- client.py
+++ client.py
@@ -1,6 +1,12 @@
 import socket as s
+
 socket = s.socket(s.AF_INET, s.SOCK_STREAM)
 socket.connect(("127.0.0.1", 1028))
 
+print("You are connected to the chat.")
+
 while True:
-    socket.send(str(input()).encode())
+    string = str(input("You: "))
+    if string != "":
+        socket.send(string.encode())
+        print(socket.recv(1024).decode('utf-8'))
