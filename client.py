883cf665ab57df62c93f29d9f39369c1e0140553
Raul Carini <r.carini2003@gmail.com>
2022-10-24 20:40:46 +0200

    add config

--- client.py
+++ client.py
@@ -1,7 +1,13 @@
 import socket as s
+import json
+
+# load config file
+with open('config.json') as config:
+    config = json.load(config)
+
 
 socket = s.socket(s.AF_INET, s.SOCK_STREAM)
-socket.connect(("127.0.0.1", 1028))
+socket.connect((config["server"]["ip"], config["server"]["port"]))
 
 print("You are connected to the chat.")
 
