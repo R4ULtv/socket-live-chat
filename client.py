6e3a29f13664a22294493733f7de371884c9086e
Raul Carini <r.carini2003@gmail.com>
2022-10-31 21:55:33 +0100

    split send and recive

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket as s
+import threading as t
 import json
 
 # load config file
@@ -9,9 +10,16 @@ with open('config.json') as config:
 socket = s.socket(s.AF_INET, s.SOCK_STREAM)
 socket.connect((config["server"]["ip"], config["server"]["port"]))
 
-# client loop
-while True:
-    string = str(input("You: "))
-    if string != "":
-        socket.send(string.encode('utf-8'))
-        # print(socket.recv(1024).decode('utf-8'))
+
+def send():
+    while True:
+        print(socket.recv(1024).decode('utf-8'))
+
+def recive():
+    while True:
+        string = str(input("You: "))
+        if string != "":
+            socket.send(string.encode('utf-8'))
+
+t.Thread(target=send).start()
+t.Thread(target=recive).start()
